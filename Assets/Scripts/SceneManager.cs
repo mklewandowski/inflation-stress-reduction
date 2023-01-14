17adf13d8fc07c9041358bde4800c8951f98991d
mklewandowski <matt.kain.lewandowski@gmail.com>
2023-01-14 00:19:45 -0800

    Connect item HUD to scene manager and add function to init item

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SceneManager : MonoBehaviour
 {
@@ -19,10 +21,33 @@ public class SceneManager : MonoBehaviour
     [SerializeField]
     ParticleSystem ParticlesCoin;
 
+    [SerializeField]
+    Sprite[] ItemSprites;
+    [SerializeField]
+    string[] ItemNames;
+    [SerializeField]
+    int[] ItemIdealPrices;
+
+    [SerializeField]
+    TextMeshProUGUI HUDItemName;
+    [SerializeField]
+    GameObject HUDItemImage;
+    [SerializeField]
+    TextMeshProUGUI HUDItemPrice;
+    [SerializeField]
+    GameObject HUDInstructions;
+    [SerializeField]
+    GameObject HUDDeflated;
+    [SerializeField]
+    GameObject HUDNext;
+
     bool startingIntro = false;
 
     float particleTimer = 0f;
     float particleTimerMax = .5f;
+
+    int itemIndex = 0;
+    int currentPrice = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +56,17 @@ public class SceneManager : MonoBehaviour
         HUDFooter.GetComponent<MoveNormal>().MoveUp();
         HUDStart.GetComponent<MoveNormal>().MoveLeft();
         startingIntro = true;
+
+        SetItem();
+    }
+
+    void SetItem()
+    {
+        itemIndex = Random.Range(0, ItemNames.Length);
+        currentPrice = Random.Range(ItemIdealPrices[itemIndex] * 30, ItemIdealPrices[itemIndex] * 50);
+        HUDItemPrice.text = "Price: $" + currentPrice;
+        HUDItemName.text = ItemNames[itemIndex];
+        HUDItemImage.GetComponent<Image>().sprite = ItemSprites[itemIndex];
     }
 
     // Update is called once per frame
@@ -63,8 +99,13 @@ public class SceneManager : MonoBehaviour
 
     public void Deflate()
     {
+        HUDItemImage.GetComponent<GrowAndShrink>().StartEffect();
         ParticlesCash.Play();
         ParticlesCoin.Play();
         particleTimer = particleTimerMax;
+
+        int priceDecrement = Random.Range(ItemIdealPrices[itemIndex] * 1, ItemIdealPrices[itemIndex] * 5);
+        currentPrice = Mathf.Max(currentPrice - priceDecrement, ItemIdealPrices[itemIndex]);
+        HUDItemPrice.text = "Price: $" + currentPrice;
     }
 }
