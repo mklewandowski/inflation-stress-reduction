cd57608136855c799aeb258ecfb4bfea327ffeb2
mklewandowski <matt.kain.lewandowski@gmail.com>
2023-01-13 21:53:26 -0800

    Add intro transitions

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject HUDTitle;
+    [SerializeField]
+    GameObject HUDFooter;
+    [SerializeField]
+    GameObject HUDStart;
+
+    [SerializeField]
+    GameObject HUDItemContainer1;
+
+    bool startingIntro = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HUDTitle.GetComponent<MoveNormal>().MoveDown();
+        HUDFooter.GetComponent<MoveNormal>().MoveUp();
+        HUDStart.GetComponent<MoveNormal>().MoveLeft();
+        startingIntro = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (startingIntro && !HUDStart.GetComponent<MoveNormal>().IsMoving())
+        {
+            startingIntro = false;
+            HUDStart.GetComponent<MoveNormal>().SetNewLeftEndPos(new Vector2(-1000f, 0));
+
+        }
+    }
+
+    public void StartGame()
+    {
+        HUDTitle.GetComponent<MoveNormal>().MoveUp();
+        HUDFooter.GetComponent<MoveNormal>().MoveDown();
+        HUDStart.GetComponent<MoveNormal>().MoveLeft();
+        HUDItemContainer1.GetComponent<MoveNormal>().MoveLeft();
     }
 }
